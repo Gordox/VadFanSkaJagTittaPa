d555b007689c45a2efc81546fb56bdb486efc637
Marcus Lindstedt <marcus.t.lindstedt@gmail.com>
2016-10-28 12:02:56 +0200

    Added ID for Malmö and started json parse.

--- VadFanSkaJagTittaPa/app/src/main/java/gg/gordox/vadfanskajagtittapa/Weather.java
+++ VadFanSkaJagTittaPa/app/src/main/java/gg/gordox/vadfanskajagtittapa/Weather.java
@@ -1,23 +1,13 @@
 package gg.gordox.vadfanskajagtittapa;
 
-import android.app.Activity;
-import android.net.ConnectivityManager;
-import android.net.NetworkInfo;
-import android.os.AsyncTask;
-import android.util.Log;
-import android.widget.Toast;
-
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.BufferedInputStream;
-import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.util.Scanner;
 
 /** 665223979558-gq5j986cmisaa9v2717sq085vl35qfbb.apps.googleusercontent.com
  * http://openweathermap.org/current
@@ -34,7 +24,7 @@ public class Weather {
 
     private static final float KELVIN_OFFSET = -273.15f;
     private static final String API_KEY = "6039dfdf85eb9e8486e84a5753bc019b";
-    private Pair<Float, Float> MALMÖ = new Pair<>(55.5872199f, 12.9304129f);
+    private Pair<Float, Float> MALMÖ = new Pair<>(55.5872199f, 12.9304129f); // Not needed if we fetch using ID
     private Pair<Float, Float> pos;
 
     public Weather() {
@@ -45,12 +35,28 @@ public class Weather {
         int a = (int)pos.getFirst().floatValue();
         int b = (int)pos.getSecond().floatValue();
 
+        URL idUrl = new URL("api.openweathermap.org/data/2.5/weather?id=2712995&APPID=" + API_KEY); //ID for Malmö, Sweden
         URL url = new URL("api.openweathermap.org/data/2.5/weather?q=London&APPID=" + API_KEY);
         HttpURLConnection connection = (HttpURLConnection) url.openConnection();
         InputStream input = new BufferedInputStream(connection.getInputStream());//connection.getInputStream();
         String str = "";
 
+        //String data = "";
+
         JSONObject obj = new JSONObject(str);
+        //JSONObject temp = obj.getJSONObject("weather");
+        /*
+        JSONArray jsonArray = obj.optJSONArray("weather");
+
+        for(int i = 0; i < jsonArray.length(); i++)
+        {
+            JSONObject jsonObject = jsonArray.getJSONObject(i);
+
+            String weatherType = jsonObject.optString("main").toString();
+
+            data += "Weather: " + weatherType;
+        }
+        */
 
         return obj;
     }
